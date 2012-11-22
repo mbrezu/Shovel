283c8f95e230720509358107815381b0111e79db
Miron Brezuleanu <mbrezu@gmail.com>
2012-11-22 14:41:04 +0200

    C# implementation: better Stack.Push.

--- csharp/NShovel/Shovel/Vm/Stack.cs
+++ csharp/NShovel/Shovel/Vm/Stack.cs
@@ -2,10 +2,10 @@ using System;
 
 namespace Shovel
 {
-	public class Stack
-	{
-		Value[] storage;
-		int length;
+    public class Stack
+    {
+        Value[] storage;
+        int length;
 
         public Value[] GetUsedStack ()
         {
@@ -14,22 +14,22 @@ namespace Shovel
             return result;
         }
 
-		public Stack ()
-		{
-			this.storage = new Value[1024];
-		}
+        public Stack ()
+        {
+            this.storage = new Value[1024];
+        }
 
-        public Stack(Value[] existingValues) 
+        public Stack (Value[] existingValues)
         {
             this.storage = new Value[existingValues.Length * 2];
             this.length = existingValues.Length;
             Array.Copy (existingValues, this.storage, this.length);
         }
 
-		public Value Top ()
-		{
-			return this.storage [this.length - 1];
-		}
+        public Value Top ()
+        {
+            return this.storage [this.length - 1];
+        }
 
         public Value UnderTop (int i)
         {
@@ -41,74 +41,68 @@ namespace Shovel
             return this.storage [this.length - 2];
         }
 
-        public void UnderPopOneAndCopyTop()
+        public void UnderPopOneAndCopyTop ()
         {
-            this.storage[this.length - 2] = this.storage[this.length - 1];
+            this.storage [this.length - 2] = this.storage [this.length - 1];
             this.length --;
         }
 
         public void UnderPopAndCopyTop (int i)
         {
-            this.storage[this.length - i - 1] = this.storage[this.length - 1];
+            this.storage [this.length - i - 1] = this.storage [this.length - 1];
             this.length -= i;
         }
 
-		public Value PopTop ()
-		{
+        public Value PopTop ()
+        {
             this.length --;
-            return this.storage[length];
-		}
+            return this.storage [length];
+        }
 
-		public void Pop()
-		{
-			this.length --;
-		}
+        public void Pop ()
+        {
+            this.length --;
+        }
 
-		public void Push (Value value)
-		{
-			if (this.length < this.storage.Length) {
-				this.storage [this.length] = value;
-				this.length++;
-			} else {
-                // FIXME: use Array.Resize.
-                // Check for other places where Array.Resize may be useful.
-				var newStorage = new Value[this.storage.Length * 2];
-				Array.Copy (this.storage, newStorage, this.length);
-				this.storage = newStorage;
-				this.Push(value);
-			}
-		}
+        public void Push (Value value)
+        {
+            if (this.length == this.storage.Length) {
+                Array.Resize (ref this.storage, this.storage.Length * 2);
+            }
+            this.storage [this.length] = value;
+            this.length++;
+        }
 
         public int Count {
-			get { return this.length;}
-		}
+            get { return this.length;}
+        }
 
-		public Value[] Storage {
-			get { return this.storage; }
-		}
+        public Value[] Storage {
+            get { return this.storage; }
+        }
 
-		public void RemoveRange(int position, int rangeLength)
-		{
-			Array.Copy(this.storage, position + rangeLength, this.storage, position, rangeLength);
-			this.length -= rangeLength;
-		}
+        public void RemoveRange (int position, int rangeLength)
+        {
+            Array.Copy (this.storage, position + rangeLength, this.storage, position, rangeLength);
+            this.length -= rangeLength;
+        }
 
-		public void PopMany(int n)
-		{
-			this.length -= n;
-		}
+        public void PopMany (int n)
+        {
+            this.length -= n;
+        }
 
-		public void GetTopRange(int n, out Value[] array, out int start) 
-		{
-			array = this.storage;
-			start = this.length - n;
-		}
+        public void GetTopRange (int n, out Value[] array, out int start)
+        {
+            array = this.storage;
+            start = this.length - n;
+        }
 
         public bool TopIsReturnAddress ()
         {
-            return this.storage[this.length - 1].Kind == Value.Kinds.ReturnAddress;
+            return this.storage [this.length - 1].Kind == Value.Kinds.ReturnAddress;
         }
 
-	}
+    }
 }
 
